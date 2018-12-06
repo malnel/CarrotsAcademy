e9cc238cf81fdf0e91ac6a5d8d5ffbc2a2d7770f
Gosia <malgorzata.nelson@gmail.com>
2018-12-06 09:20:24 +0100

    Task 4.1 - przekazywanie argumentow do metody main

---
+++ Task.4.1/src/Test.java
@@ -0,0 +1,10 @@
+public class Test {
+
+    public static void main(String[] args) {
+
+        for (String arg : args) {
+            System.out.println(arg);
+        }
+    }
+}
+//Run -> Edit Configurations... -> Program arguments -> wpisz tu rozne slowa
--- TravelOffice/src/pl/akademia/Date.java
+++ TravelOffice/src/pl/akademia/Date.java
@@ -18,4 +18,6 @@ public class Date {
                 ", day=" + day +
                 '}';
     }
+
+
 }
