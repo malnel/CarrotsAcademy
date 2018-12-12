67ef2575ceb427564dad4f5ba28a9fea67d7f173
Gosia <malgorzata.nelson@gmail.com>
2018-12-12 10:56:53 +0100

    create my own collaborator to inject

--- MojeRefleksje/src/Main.java
+++ MojeRefleksje/src/Main.java
@@ -1,7 +1,6 @@
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
+import java.lang.annotation.*;
+import java.lang.reflect.AnnotatedArrayType;
+import java.lang.reflect.Field;
 
 public class Main {
 
@@ -11,9 +10,64 @@ public class Main {
 }
 
 
-//TODO 1 własna adnotacja
-@Target(ElementType.FIELD)
+// TODO 1 własna adnotacja
+@Target(ElementType.FIELD) //tutaj definiuje jakiego elementu moze dotyczyc moja wlasna adnotacja
 @Retention(RetentionPolicy.RUNTIME)
 @interface Wstrzyknij{
 
+}
+
+
+// TODO 2 klasa obiektu do wstrzykiwania (ten obiekt bede wstrzykiwac do innej klasy)
+class Collaborator {
+    private String name;
+
+    public Collaborator(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Collaborator{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+}
+
+// TODO 3 Klasa z polami typu Collaborator
+class UseCollaborator {
+
+    @Wstrzyknij
+    private Collaborator collaboratorWithAnnotation;
+
+    @Wstrzyknij
+    private Collaborator collaborator;
+
+    @Override
+    public String toString() {
+        return "UseCollaborator{" +
+                "collaboratorWithAnnotation=" + collaboratorWithAnnotation +
+                ", collaborator=" + collaborator +
+                '}';
+    }
+}
+
+class Kontener {
+    static void wstrzyknij(Object target, String name) throws IllegalAccessException {
+
+        // TODO 4 pobierz pola obiektu i przeiteruj po nich
+        Field[] declaredFields = target.getClass().getDeclaredFields();
+
+        for (Field field : declaredFields) {
+            // TODO 5 pobierz adnotacje pola i przeiteruje po nich
+            Annotation[] annotations = field.getAnnotations();
+            for (Annotation annotation : annotations) {
+                // TODO 6 utwórz instancję Collaborator i przypisz pola z adnotacją wstrzyknij
+                Collaborator collaborator = new Collaborator(name);
+                if ("Wstrzyknij".equals(annotation.annotationType().getName())) {
+                    field.set(target, new Collaborator("Wstrzyknięty collaborator"));
+                }
+            }
+        }
+    }
 }
